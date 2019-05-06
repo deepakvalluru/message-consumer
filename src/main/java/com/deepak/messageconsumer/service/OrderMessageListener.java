5e38cd5c29c93f971129f870210d64bb852b4f32
Deepu <Deepu@DESKTOP-VA5DMJC>
2019-05-06 02:25:22 -0500

    dead letter queue implementation

--- src/main/java/com/deepak/messageconsumer/service/OrderMessageListener.java
+++ src/main/java/com/deepak/messageconsumer/service/OrderMessageListener.java
@@ -2,6 +2,7 @@ package com.deepak.messageconsumer.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +18,18 @@ public class OrderMessageListener
    public void processPriorityOrder( Order order )
    {
       logger.debug( "Priority Order received : {}", order );
+      if( order.getAmount() == 20.04 )
+      {
+         // payload is requeued 3 times ( as per config ) and then moved to the Dead Letter Queue
+         // After configured TTL is over, the message is requeued back to the priority orders queue again.
+         logger.debug( "RETRYING AGAIN AFTER A FEW SECONDS" );
+         throw new RuntimeException( "RETRY AGAIN" );
+      }
+      else if( order.getAmount() == 0 )
+      {
+         logger.debug( "DO NOT REQUEUE" );
+         throw new AmqpRejectAndDontRequeueException( "DO NOT RETRY" );
+      }
    }
 
    @RabbitListener ( queues = RabbitConfig.QUEUE_ORDERS_NONPRIORITY )
